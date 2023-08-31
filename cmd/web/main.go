ac09d637bf37c16fa34329d34e8740d790b266b9
Dmytro Andrieiev <63021874+AndrieievDmytro@users.noreply.github.com>
2023-08-31 13:34:23 -0400

    .gitignore added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"WebApp/pkg/handlers"
+	"flag"
 	"fmt"
 	"net/http"
 )
 
-const portNumber = ":8080"
+var (
+	portNumber string
+)
+
+func init() {
+	flag.StringVar(&portNumber, "p", portNumber, "Port to run the application on")
+}
 
 func main() {
+	flag.Parse()
+	fmt.Printf("Starting application on port %s", portNumber)
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
-
-	fmt.Printf("Starting application on port %s", portNumber)
 
 	_ = http.ListenAndServe(portNumber, nil)
 }
