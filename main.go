45efda812621e5bfdf0ff79e2e0da55634575654
Dmytro Andrieiev <63021874+AndrieievDmytro@users.noreply.github.com>
2023-08-30 22:11:19 -0400

    Home and About Handle Function Added

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You are on a home page")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := AddValues(2, 2)
+	_, _ = fmt.Fprintf(w, "This is page about 2 + 2 and 2 + 2 is %d", sum)
+
+}
+
+func AddValues(x, y int) int {
+	return x + y
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	_ = http.ListenAndServe(portNumber, nil)
 }
